87ee8e815565b97a51a21f485f86c121e28bc721
Jaziel Lopez <juan.jaziel@gmail.com>
2020-02-18 17:29:06 -0800

    parse repository contents

--- src/repository.py
+++ src/repository.py
@@ -6,16 +6,79 @@ class Repository(object):
 
         definition = None
 
+    MAGIC_SEP = "->"
+
     path = None
 
     def __init__(self, path=None):
 
         self.path = path
 
+        self.cards = list()
+
+    def read(self):
+
+        """
+        read repository file
+        :return:
+        """
+
+        with open(self.path, "r") as source:
+
+            content = source.readlines()
+
+        return content
+
+    def parse(self, content):
+
+        """
+        parse content
+        :param content:
+        :return:
+        """
+        flush = False
+        body_term = ""
+        body_definition = ""
+
+        for statement in content:
+
+            extract = statement.split(self.MAGIC_SEP)
+
+            if len(extract) > 1:
+                if flush:
+                    self.persist(term=body_term, definition=body_definition)
+
+                body_term = extract[0]
+                body_definition = "\n>>\t" + "".join(extract[1:])
+                flush = True
+                continue
+
+            body_definition += "\t" + statement.strip(" ")
+
+        if flush:
+            self.persist(term=body_term, definition=body_definition)
+
+    def persist(self, term=None, definition=None):
+
+        """
+        store term definition pair of values into memory
+        :param term:
+        :param definition:
+        :return:
+        """
+
+        item = Repository.Item()
+        item.term = term
+        item.definition = definition
+
+        self.cards.append(item)
+
     def load(self):
 
-        elements = list()
+        """
+        load term definitions into memory
+        :return:
+        """
+        self.parse(self.read())
 
-        elements.append(Repository.Item())
-
-        return elements
+        return self.cards
--- test/repository.py
+++ test/repository.py
@@ -1,5 +1,7 @@
+from os import path, access, R_OK, getcwd
 import unittest
 from src.repository import Repository
+from unittest.mock import MagicMock
 
 
 class TestRepository(unittest.TestCase):
@@ -11,12 +13,21 @@ class TestRepository(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        self.repo = Repository("foo")
+        self.repo = Repository(path.join(getcwd(),"..", "sample", "repository.txt"))
         self.item = Repository.Item()
 
-    def test_property_path_not_empty(self):
+    def test_path_not_empty(self):
 
         self.assertIsNotNone(self.repo.path)
+
+    def test_path_readable(self):
+
+        self.assertTrue(path.exists(self.repo.path))
+        self.assertTrue(access(self.repo.path, R_OK))
+
+    def test_read_find_content(self):
+
+        self.assertGreater(len(self.repo.read()), 0)
 
     def test_load_not_empty(self):
 
@@ -28,9 +39,9 @@ class TestRepository(unittest.TestCase):
 
     def test_load_is_list_with_term_definition_items(self):
 
-        for i in self.repo.load():
+        for item in self.repo.load():
 
-            self.assertIsInstance(i, self.item.__class__)
+            self.assertIsInstance(item, Repository.Item().__class__)
 
 if __name__ == "__main__":
     unittest.main()
